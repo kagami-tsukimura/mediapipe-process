be4f8006abcaeabd264a35e07d3664d125639c37
Kagami <shimura.kyohei7617@gmail.com>
2024-07-22 20:47:37 +0900

    auto: 2024年  7月 22日 月曜日 20:47:37 JST

--- opencv/open_cv.py
+++ opencv/open_cv.py
@@ -1,6 +1,7 @@
 import cv2
-import matplotlib.pyplot as plt
 
 image = cv2.imread("./images/icon.png")
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.show()
+cv2.imshow("", image)
+
+cv2.waitKey()
+cv2.destroyAllWindows()

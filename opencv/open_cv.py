f045fdebeaff105c9dbcf45c5a691df053b84686
Kagami <shimura.kyohei7617@gmail.com>
2024-07-22 23:57:01 +0900

    auto: 2024年  7月 22日 月曜日 23:57:01 JST

--- opencv/open_cv.py
+++ opencv/open_cv.py
@@ -7,21 +7,23 @@ img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 imgs = {"img": img, "img_gray": img_gray, "img_hsv": img_hsv, "img_rgb": img_rgb}
 
+# 生の画像データ取得
 w, h, c = img.shape
 print(f"width: {w}, height: {h}, channels: {c}")
 print(img[100, 100])
-cv2.circle(img, (100, 100), 10, (0, 0, 255), -1)
-cv2.putText(
-    img,
-    "Penguin",
-    (w // 3 * 1, h // 7 * 6),
-    cv2.FONT_HERSHEY_DUPLEX,
-    1,
-    (255, 0, 0),
-    2,
-)
+
 
 for name, img in imgs.items():
+    cv2.circle(img, (100, 100), 10, (0, 0, 255), -1)
+    cv2.putText(
+        img,
+        name,
+        (w // 3 * 1, h // 7 * 6),
+        cv2.FONT_HERSHEY_DUPLEX,
+        1,
+        (255, 0, 0),
+        2,
+    )
     cv2.imshow("", img)
 
     cv2.waitKey()

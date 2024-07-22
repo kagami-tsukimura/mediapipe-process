5f173dd02df95e61a3981aae0df7659600045dbc
Kagami <shimura.kyohei7617@gmail.com>
2024-07-22 20:51:14 +0900

    auto: 2024年  7月 22日 月曜日 20:51:14 JST

--- opencv/open_cv.py
+++ opencv/open_cv.py
@@ -1,6 +1,16 @@
 import cv2
 
 image = cv2.imread("./images/icon.png")
+w, h, _ = image.shape
+cv2.putText(
+    image,
+    "Penguin",
+    (w // 3 * 1, h // 7 * 6),
+    cv2.FONT_HERSHEY_DUPLEX,
+    1,
+    (255, 0, 0),
+    2,
+)
 cv2.imshow("", image)
 
 cv2.waitKey()

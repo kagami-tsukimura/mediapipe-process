c1a2fd8c36cfb87e2cf2cbf325621e4be615c407
Kagami <shimura.kyohei7617@gmail.com>
2024-07-23 20:53:51 +0900

    auto: 2024年  7月 23日 火曜日 20:53:51 JST

--- opencv/movie_open_cv.py
+++ opencv/movie_open_cv.py
@@ -3,6 +3,8 @@ import cv2
 movie_path = "./videos/movie1.mp4"
 
 cap = cv2.VideoCapture(movie_path)
+
+# 動画読み込み失敗時
 if not cap.isOpened():
     raise FileNotFoundError(f"動画ファイル {movie_path} が見つかりませんでした。")
 

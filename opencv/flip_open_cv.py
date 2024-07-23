6d132813f3d141a8a2f8fd0c66fb616c4e4ca3f6
Kagami <shimura.kyohei7617@gmail.com>
2024-07-23 14:29:38 +0900

    auto: 2024年  7月 23日 火曜日 14:29:38 JST

--- opencv/flip_open_cv.py
+++ opencv/flip_open_cv.py
@@ -2,10 +2,13 @@ import cv2
 
 img = cv2.imread("./images/icon.png")
 
+# 回転
 # 時計回りに90度回転
 clock_img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
 # 反時計回りに90度回転
 counter_img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
 # 180度回転
 reverse_img = cv2.rotate(img, cv2.ROTATE_180)
 
@@ -18,12 +21,26 @@ scale = 1
 trans = cv2.getRotationMatrix2D(center, angle, scale)
 affine_img = cv2.warpAffine(img, trans, (width, height))
 
+# 反転
+# 上下反転
+ver_flip_img = cv2.flip(img, 0)
+
+# 左右反転
+hol_flip_img = cv2.flip(img, 1)
+
+# 上下左右反転
+verhol_flip_img = cv2.flip(img, -1)
+
+
 imgs = {
     "origin": img,
     "clock": clock_img,
     "counter": counter_img,
     "reverse": reverse_img,
     "affine": affine_img,
+    "ver_flip": ver_flip_img,
+    "hol_flip": hol_flip_img,
+    "verhol_flip": verhol_flip_img,
 }
 
 for name, image in imgs.items():
